80b0de7368528a4b4f5ed7658c1c47a4966174a6
erickogi <erickogi14@gmail.com>
2024-09-02 15:39:54 +0300

    Fix title

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="container mx-auto px-4 py-8">
     <div class="flex items-center justify-center mb-8">
       <img src="../public/codematics-logo.png" alt="Codematic Logo" class="h-12 mr-4" />
-      <h1 class="text-3xl font-bold text-blue-800">Codematics Video Details</h1>
+      <h1 class="text-3xl font-bold text-blue-800">Video Details</h1>
     </div>
     <div class="mb-8">
       <input v-model="videoInput" @keyup.enter="processVideoInput" placeholder="Enter YouTube video ID or URL"
